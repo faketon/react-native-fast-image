41141fc0ab3ddf0918d461788774eb498e0843c4
faketon <chalernponman@hotmail.com>
2018-06-04 18:01:07 +0700

    Update FastImageViewModule.java

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
@@ -40,21 +40,22 @@ class FastImageViewModule extends ReactContextBaseJavaModule {
                     final ReadableMap source = sources.getMap(i);
                     final GlideUrl glideUrl = FastImageViewConverter.glideUrl(source);
                     final Priority priority = FastImageViewConverter.priority(source);
+                    
+                    RequestOptions options = new RequestOptions()
+                            .placeholder(TRANSPARENT_DRAWABLE)
+                            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+                            .priority(priority);
                     if (glideUrl.toStringUrl().startsWith("http://") || glideUrl.toStringUrl().startsWith("https://")) {
                         Glide
                                 .with(activity.getApplicationContext())
+                                .applyDefaultRequestOptions(options)
                                 .load(glideUrl)
-                                .priority(priority)
-                                .placeholder(TRANSPARENT_DRAWABLE)
-                                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
                                 .preload();
                     } else {
                         Glide
                                 .with(activity.getApplicationContext())
-                                .load(glideUrl.toStringUrl())
-                                .priority(priority)
-                                .placeholder(TRANSPARENT_DRAWABLE)
-                                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                                .applyDefaultRequestOptions(options)
+                                .load(glideUrl)
                                 .preload();
                     }
                 }
