91abdc8c31e867b90d336c49aa9773340bf29135
faketon <chalernponman@hotmail.com>
2018-06-04 17:35:45 +0700

    Update FastImageViewModule.java

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
@@ -9,7 +9,6 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.Priority;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.model.GlideUrl;
-import com.bumptech.glide.request.RequestOptions;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -34,7 +33,6 @@ class FastImageViewModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void preload(final ReadableArray sources) {
         final Activity activity = getCurrentActivity();
-        if (activity == null) return;
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -42,17 +40,23 @@ class FastImageViewModule extends ReactContextBaseJavaModule {
                     final ReadableMap source = sources.getMap(i);
                     final GlideUrl glideUrl = FastImageViewConverter.glideUrl(source);
                     final Priority priority = FastImageViewConverter.priority(source);
-
-                    RequestOptions options = new RequestOptions()
-                            .placeholder(TRANSPARENT_DRAWABLE)
-                            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
-                            .priority(priority);
-
-                    Glide
-                            .with(activity.getApplicationContext())
-                            .applyDefaultRequestOptions(options)
-                            .load(glideUrl)
-                            .preload();
+                    if (glideUrl.toStringUrl().startsWith("http://") || glideUrl.toStringUrl().startsWith("https://")) {
+                        Glide
+                                .with(activity.getApplicationContext())
+                                .load(glideUrl)
+                                .priority(priority)
+                                .placeholder(TRANSPARENT_DRAWABLE)
+                                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                                .preload();
+                    } else {
+                        Glide
+                                .with(activity.getApplicationContext())
+                                .load(glideUrl.toStringUrl())
+                                .priority(priority)
+                                .placeholder(TRANSPARENT_DRAWABLE)
+                                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                                .preload();
+                    }
                 }
             }
         });
